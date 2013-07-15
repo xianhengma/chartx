0f66125ac9b720ed5978a1b557c42379a49341ba
Xianheng Ma <xianheng.ma@gmail.com>
2013-07-15 16:30:12 -0700

    Add stacked_area chart

--- lib/chartx/helper.rb
+++ lib/chartx/helper.rb
@@ -5,7 +5,7 @@ module Chartx
   module Helper
     
     #pie_chart has different input data format LoL
-    #Todo: fix the input data format
+    #TODO: fix the input data format
     def pie_chart(data_source, options = {})
       options = options.dup
       @chartx_id ||= 0
@@ -56,7 +56,7 @@ JS
       
     end
     
-    def bar_chart(data_source, options ={})
+    def bar_chart(data_source, options = {})
       options = options.dup
       @chartx_id ||= 0
       height = options.delete(:height) || "600"
@@ -107,7 +107,7 @@ JS
   
     end
     
-    def line_chart(data_source, options ={}) 
+    def line_chart(data_source, options = {}) 
     
       options = options.dup
       @chartx_id ||= 0
@@ -165,7 +165,7 @@ JS
     
     end
     
-    def scatter_chart(data_source, options ={})
+    def scatter_chart(data_source, options = {})
       options = options.dup
       @chartx_id ||= 0
       height = options.delete(:height) || "600"
@@ -212,6 +212,61 @@ JS
       end
 
       html.respond_to?(:html_safe) ? html.html_safe : html
+      
+    end
+    
+    def stacked_area_chart(data_source, options = {})
+      options = options.dup
+      @chartx_id ||= 0
+      height = options.delete(:height) || "600"
+      width = options.delete(:width) || "500"
+      elem_id = options.delete(:id) || "chart-#{@chartx_id += 1}"
+      
+      html = <<HTML
+      <div id="#{ERB::Util.html_escape(elem_id)}" style="height: #{ERB::Util.html_escape(height)}px;">
+      <svg></svg>
+      </div>
+HTML
+ 
+      js = <<JS
+      
+      <script type="text/javascript">
+      
+      stack_area_data = #{data_source.to_json}; 
+      
+      nv.addGraph(function() {
+        var chart = nv.models.stackedAreaChart()
+                    .x(function(d) { return d.x })
+                    .y(function(d) { return d.y })
+                    .clipEdge(true);
+
+        chart.xAxis
+            //.showMaxMin(false)
+            .tickFormat(d3.format('.02f'));
+            //.tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+
+        chart.yAxis
+            .tickFormat(d3.format('.02f'));
+
+        d3.select("##{elem_id} svg")
+          .datum(stack_area_data)
+            .transition()
+            .duration(500)
+            .call(chart);
+            
+        return chart;
+      });
+
+      </script>
+JS
+      if options[:content_for]
+        content_for(options[:content_for]) { js.respond_to?(:html_safe) ? js.html_safe : js }
+      else
+        html += js
+      end
+
+      html.respond_to?(:html_safe) ? html.html_safe : html
+      
       
     end
   end
