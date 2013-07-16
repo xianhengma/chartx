8fc47c7340fcf37d81b8e3ec0ac15f4b2be4dfff
Xianheng Ma <xianheng.ma@gmail.com>
2013-07-15 17:05:59 -0700

    Add Line with View Finder Chart

--- lib/chartx/helper.rb
+++ lib/chartx/helper.rb
@@ -41,6 +41,7 @@ HTML
                  .attr('height', #{height})
                  .call(chart);
 
+           nv.utils.windowResize(chart.update);
            return chart;
        });
        
@@ -92,6 +93,7 @@ HTML
             .attr('width', #{width})
             .attr('height', #{height})
             .call(chart);
+        nv.utils.windowResize(chart.update);    
 
         return chart;
       });
@@ -150,6 +152,7 @@ HTML
           .attr('height', #{height})
             .call(chart);
 
+        nv.utils.windowResize(chart.update);
         return chart;
       });
       
@@ -163,6 +166,63 @@ JS
 
       html.respond_to?(:html_safe) ? html.html_safe : html
     
+    end
+    
+    def line_with_view_finder_chart(data_source, options = {})
+
+      
+      options = options.dup
+      @chartx_id ||= 0
+      height = options.delete(:height) || "600"
+      width = options.delete(:width) || "500"
+      elem_id = options.delete(:id) || "chart-#{@chartx_id += 1}"
+      
+      x_axis_label = options.delete(:x_axis_label) || "X(Unit)"
+      
+      html = <<HTML
+      <div id="#{ERB::Util.html_escape(elem_id)}" style="height: #{ERB::Util.html_escape(height)}px;">
+      <svg></svg>
+      </div>
+HTML
+
+      js = <<JS
+      
+      <script type="text/javascript">
+      var chart;
+      
+      nv.addGraph(function() {
+        var chart = nv.models.lineWithFocusChart();
+
+        chart.xAxis
+            .tickFormat(d3.format(',f'));
+
+        chart.yAxis
+            .tickFormat(d3.format(',.2f'));
+
+        chart.y2Axis
+            .tickFormat(d3.format(',.2f'));
+
+        d3.select("##{elem_id} svg")
+            .datum(#{data_source.to_json})
+          .transition().duration(500)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+      });
+      
+      </script>
+JS
+      if options[:content_for]
+        content_for(options[:content_for]) { js.respond_to?(:html_safe) ? js.html_safe : js }
+      else
+        html += js
+      end
+
+      html.respond_to?(:html_safe) ? html.html_safe : html
+    
+      
     end
     
     def scatter_chart(data_source, options = {})
@@ -199,6 +259,8 @@ HTML
             .datum(scatter_input_data)
           .transition().duration(500)
             .call(chart);
+            
+        nv.utils.windowResize(chart.update);
             
         return chart;
       });
@@ -253,7 +315,7 @@ HTML
             .transition()
             .duration(500)
             .call(chart);
-            
+        nv.utils.windowResize(chart.update);    
         return chart;
       });
 
