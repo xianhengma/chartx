0303a0b86a6c942650873d4edede88e144a95bbf
Xianheng Ma <xianheng.ma@gmail.com>
2013-07-16 21:49:28 -0700

    change the charts to core and models

---
+++ vendor/assets/javascripts/chartx-core.js
@@ -0,0 +1,9 @@
+// This is a manifest file that'll be compiled into chartx-core.js, which will include all the files
+// listed below.
+// Please make sure you include this file before using Chartx
+//
+
+//= require ./nvd3/lib/d3.min
+//= require ./nvd3/nv.d3.min
+//= require ./nvd3/src/utils.js
+//= require set-env.js.erb
---
+++ vendor/assets/javascripts/chartx-models.js
@@ -0,0 +1,2 @@
+// When you include this file, you will include all the models of Chartx
+//= require_tree ./nvd3/src/models
